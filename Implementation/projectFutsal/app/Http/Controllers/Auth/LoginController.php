8aff9415f09f52abc18bb6b53f8f7fca28f9a898
uttamtamang <uttamtamang45@gmail.com>
2019-03-07 17:19:11 +0545

    Adding Laravel Project in Implimentation Phase

---
+++ Implementation/projectFutsal/app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Auth;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    //protected $redirectTo = '/home';
+    public function redirectTo()
+    {
+        $role=Auth::user()->usertype;
+        switch($role){
+        case '0':
+        return ('/home');
+        break;
+        case '1':
+        return ('/admindash');
+        break;
+        default:
+        return ('welcome');
+        break;
+        }
+
+    }
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+}
---
+++ Implementation/projectFutsal/database/migrations/2019_03_07_070547_rate_time.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RateTime extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('rate_time', function (Blueprint $table) {
+            $table->increments('rate_time_id');
+            $table->string('time');
+            $table->integer('ground_id')->unsigned();
+            $table->foreign('ground_id')->references('ground_id')->on('ground');
+            $table->double('rate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('rate_time');
+    }
+}
